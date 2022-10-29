e1c3baa70be2dca2fa2c81eb508726f5428c6e43
anjikc <anjikchowdhury@gmail.com>
2022-10-29 16:33:46 -0500

    changes

--- starter/setup.py
+++ starter/setup.py
@@ -1,8 +1,8 @@
 import setuptools
 
 setuptools.setup(
-    name="starter",
+    name="Heroku-Fast-API-ML",
     version="0.0.0",
-    description="Starter code.",
-    author="Student",
+    description="Code for Ml Heroku deployment with Fast API ",
+    author="Anjik Chowdhury",
 )
